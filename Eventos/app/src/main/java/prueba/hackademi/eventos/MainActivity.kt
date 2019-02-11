7e994ac4060a8039ea75e8c4277a7d9992fd541c
JoaquinIMT <A01740395@itesm.mx>
2019-02-11 16:46:30 -0700

    Cambio para nueva api

--- Eventos/app/src/main/java/prueba/hackademi/eventos/MainActivity.kt
+++ Eventos/app/src/main/java/prueba/hackademi/eventos/MainActivity.kt
@@ -12,7 +12,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.IOException
 
-val url = "http://127.0.0.1:8000/eventos/detalles_evento/   "
+val url = "http://127.0.0.1:8000/eventos/detalles_evento/"
 
 class MainActivity : AppCompatActivity() {
 
